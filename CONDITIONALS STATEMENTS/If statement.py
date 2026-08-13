fa6103bba635c0cb4e9831846e977909f8ce33ff
MD <mohk0506@gmail.com>
2026-08-13 20:06:29 +0530

    Learn logical operators

---
+++ CONDITIONALS STATEMENTS/If statement.py
@@ -0,0 +1,36 @@
+
+'''
+What are Conditional Statements?
+
+    Conditional statements allow you to execute code based on certain conditions.
+    Python uses if, elif, and else for decision-making.
+'''
+ # Syntax
+'''
+if condition1:
+    # Code to execute if condition1 is True
+elif condition2:
+    # Code to execute if condition2 is True
+else:
+    # Code to execute if all conditions are False
+
+'''
+age = 18
+
+if age < 18:
+    print("You are a minor.")
+elif age == 18:
+    print("You just became an adult!")
+else:
+    print("You are an adult.")
+
+# IF statment 
+
+age = 12
+
+if(age>18):
+    print("You can drive")
+    print("Thank you ")
+
+print("End of program")
+
--- Operators/Assignment Operators.py
+++ Operators/Assignment Operators.py
@@ -1,4 +1,11 @@
+# Example
 
+a = 32
+print (a)
+a+=3
+print(a)
+
+# Example 2
 
 x = 10
 x += 5   # Equivalent to x = x + 5
