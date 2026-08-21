cd5a24a687fb9c6f40b36f9f70467f4ca3053593
MD <mohk0506@gmail.com>
2026-08-21 19:32:55 +0530

    Add loops strings and control flow

--- Functions/Arguments.py
+++ Functions/Arguments.py
@@ -8,3 +8,23 @@ c = add(3, 5, 2)
 print(c)
 
 c1 = add(b=5, a=3)
+
+'''
+Types of Arguments:
+
+Positional Arguments:-
+def add(a, b):
+    return a + b
+print(add(5, 3))  # Output: 8
+
+Default Arguments:-
+def greet(name="Guest"):
+    return f"Hello, {name}!"
+print(greet())  # Output: Hello, Guest!
+
+Keyword Arguments:-
+def student(name, age):
+    print(f"Name: {name}, Age: {age}")
+student(age=20, name="Bob")
+
+'''
